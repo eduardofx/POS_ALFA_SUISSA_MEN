8e5cc09d8a4d6c4b56b5683bf0ec9abcecd3a120
Eduardo <eduardok.fx@gmail.com>
2017-09-02 15:01:16 -0300

    Add files via upload

---
+++ api/modules/User/controller.js
@@ -0,0 +1,13 @@
+
+const Model = require( './model' )
+
+const ACTIONS_PATH = './../../../'
+const create = require( ACTIONS_PATH + 'actions/create')(Model)
+const findOne = require( ACTIONS_PATH + 'actions/findone')(Model)
+const remove = require( ACTIONS_PATH + 'actions/remove')(Model)
+const find = require( ACTIONS_PATH + 'actions/find')(Model)
+const update = require( ACTIONS_PATH + 'actions/update')(Model)
+
+module.exports = { find,create,findOne,update,remove }
+
+//you´re hacker
